274dc443f7c863d488eeb20408f74815770105a7
David Hovemeyer <david.hovemeyer@gmail.com>
2019-06-12 16:35:02 -0400

    started to flesh out option handling, control flow

--- hostutil/eeprog.c
+++ hostutil/eeprog.c
@@ -23,13 +23,31 @@
 // StackOverflow page:
 //   https://stackoverflow.com/questions/6947413/how-to-open-read-and-write-from-serial-port-in-c
 
+#include <stdint.h>
 #include <errno.h>
 #include <fcntl.h> 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdarg.h>
 #include <string.h>
 #include <termios.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
+
+struct Data {
+	const char *fileName;
+	int fd;
+};
+
+const char *g_port, *g_fileName, *g_outputFileName;
+int g_writeProtectEnable, g_writeProtectDisable;
+int g_verify;
+off_t g_dataReadSize = -1;
+int g_showHelp;
+
+off_t g_dataSize;
+uint8_t g_dataBuf[65536];
 
 int set_interface_attribs(int fd, int speed) {
 	struct termios tty;
@@ -65,13 +83,148 @@ int set_interface_attribs(int fd, int speed) {
 	return 0;
 }
 
-const char *g_port;
-const char *g_dataFile;
-int g_disableWriteProtect;
-int g_enableWriteProtect;
+void error(const char *fmt, ...) {
+	va_list args;
+
+	va_start(args, fmt);
+	fprintf(stderr, "Error: ");
+	vfprintf(stderr, fmt, args);
+	fprintf(stderr, "\n");
+	va_end(args);
+
+	exit(1);
+}
+
+void warn(const char *fmt, ...) {
+	va_list args;
+
+	va_start(args, fmt);
+	fprintf(stderr, "Warning: ");
+	vfprintf(stderr, fmt, args);
+	fprintf(stderr, "\n");
+	va_end(args);
+}
+
+void showHelp() {
+	printf("Usage: eeprog <options>\n"
+		"Options are:\n"
+		"  -f <filename>     specify input filename\n"
+		"  -p <port>         specify comm port\n"
+		"  -o <filename>     specify output filename\n"
+		"  -r <num bytes>    specify number of bytes to read\n"
+		"  -N                enable write protection\n"
+		"  -D                disable write protection\n"
+		"  -v                verify data after writing\n"
+		"  -h                print this help text\n"
+		);
+}
+
+void *xmalloc(size_t n) {
+	void *buf = malloc(n);
+	if (!buf) { error("memory allocation failure"); }
+	return buf;
+}
+
+char *xstrdup(const char *s) {
+	char *copy = strdup(s);
+	if (!copy) { error("memory allocation failure"); }
+	return copy;
+}
+
+struct Data *data_open(const char *fileName) {
+	struct Data *dat = xmalloc(sizeof(struct Data));
+	dat->fd = open(fileName, O_RDONLY);
+	if (dat->fd < 0) { error("couldn't open data file '%s'", fileName); }
+	dat->fileName = fileName;
+	return dat;
+}
+
+off_t data_getSize(struct Data *dat) {
+	struct stat st;
+	if (fstat(dat->fd, &st) < 0) { error("couldn't get size of file '%s'", dat->fileName); }
+	return st.st_size;
+}
+
+void data_read(struct Data *dat, uint8_t *buf, off_t size) {
+	ssize_t nread = 0;
+	while (nread < size) {
+		ssize_t rc = read(dat->fd, buf + nread, size);
+		if (rc < 0) { error("error reading data from '%s'", dat->fileName); }
+		nread += rc;
+	}
+}
 
 int main(int argc, char **argv) {
-	// TODO
+	int opt;
+	while ((opt = getopt(argc, argv, "p:f:o:r:NDvh")) != -1) {
+		switch (opt) {
+		case 'p':
+			g_port = xstrdup(optarg); break;
+		case 'f':
+			g_fileName = xstrdup(optarg); break;
+		case 'o':
+			g_outputFileName = xstrdup(optarg); break;
+		case 'r':
+			{
+				int readSize;
+				if (sscanf(optarg, "%d", &readSize) != 1) {
+					error("invalid data size '%s'", optarg);
+				}
+				g_dataReadSize = readSize;
+			}
+			break;
+		case 'N':
+			g_writeProtectEnable = 1; break;
+		case 'D':
+			g_writeProtectDisable = 1; break;
+		case 'v':
+			g_verify = 1; break;
+		case 'h':
+		case '?':
+			g_showHelp = opt; break;
+		}
+	}
+
+	if (g_showHelp) {
+		showHelp();
+		exit(g_showHelp == '?' ? 1 : 0);
+	}
+
+	if (!g_port) {
+		error("comm port must be specified");
+	}
+
+	if (!g_fileName && !g_outputFileName) {
+		error("either -f or -o must be specified");
+	}
+	if (g_outputFileName && g_dataReadSize < 0) {
+		error("-r must be specified to specify read size");
+	}
+
+	struct Data *dataIn = NULL;
+
+	if (g_fileName) {
+		dataIn = data_open(g_fileName);
+		g_dataSize = data_getSize(dataIn);
+		if (g_dataSize > 65536) { error("Size of file '%s' exceeds 64K", g_fileName); }
+		data_read(dataIn, g_dataBuf, g_dataSize);
+	}
+
+	if (g_writeProtectDisable) {
+		// TODO: disable write protection
+	}
+
+	if (g_fileName) {
+		// TODO: write data to EEPROM
+	}
+
+	if (g_writeProtectEnable) {
+		// TODO: enable write protection
+	}
+
+	if (g_verify || g_outputFileName) {
+		// TODO: read data
+	}
 }
 
 // vim:ts=2:
